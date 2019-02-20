91593e575bdd01d921d9b8489bfbbdd6f537fa46
Rémi Jean <remi.jean@outlook.com>
2019-02-21 00:44:50 +0100

    Traduction du fichier layout/main/main.js

--- layout/main/main.js
+++ layout/main/main.js
@@ -1,4 +1,4 @@
-// Change the background color of nav after scrolling
+// Changer la couleur de fond de la navigation après le défilement
 $(window).on("scroll", function() {
     var navigation = $(".navigation");
     navigation.toggleClass("navigation--fixed", navigation.offset().top !== 0);
