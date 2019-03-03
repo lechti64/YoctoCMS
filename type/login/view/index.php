e91307bd71113093f4b18869a142d67c0740d6df
Rémi Jean <remi.jean@outlook.com>
2019-03-03 22:19:10 +0100

    Amélioration de l'UX du type connexion

--- type/login/view/index.php
+++ type/login/view/index.php
@@ -1,13 +1,23 @@
 <form method="post">
-    <div class="form-group">
-        <?php echo $this->getForm()->label('username', 'Identifiant'); ?>
-        <?php echo $this->getForm()->input('username'); ?>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card bg-light">
+                <div class="card-body">
+                    <div class="form-group">
+                        <?php echo $this->getForm()->label('username', 'Identifiant (admin)'); ?>
+                        <?php echo $this->getForm()->input('username'); ?>
+                    </div>
+                    <div class="form-group">
+                        <?php echo $this->getForm()->label('password', 'Mot de passe (password)'); ?>
+                        <?php echo $this->getForm()->input('password', [
+                            'type' => 'password',
+                        ]); ?>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Se connecter</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        <?php echo $this->getForm()->label('password', 'Mot de passe'); ?>
-        <?php echo $this->getForm()->input('password', [
-            'type' => 'password',
-        ]); ?>
-    </div>
-    <button type="submit" class="btn btn-primary">Se connecter</button>
 </form>
